4fc7df859e8617a91c591f4e434d42c2956aa44e
100sidedice <moreskilltreegames@gmail.com>
2026-09-22 16:33:57 -0400

    red

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 
+square = pygame.Rect(100, 100, 50, 50)
+
 def main():
     running = True
     while running:
@@ -14,6 +16,8 @@ def main():
                 running = False
 
         screen.fill((0, 0, 0))
+        # the (255,0,0) is red
+        pygame.draw.rect(screen, (255, 0, 0), square)
         pygame.display.flip()
 
     pygame.quit()
