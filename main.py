5d50285b552e725c0a16e52f6383c5f5b09b28d9
100sidedice <moreskilltreegames@gmail.com>
2026-09-22 16:27:26 -0400

    idk

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("hello world")
-print("AYLAA")
+import sys
+import pygame
+
+pygame.init()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
+
+main()
